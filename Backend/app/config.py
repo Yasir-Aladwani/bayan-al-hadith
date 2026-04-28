3dcf86d2efa08d8dcc406b2121980fc48fb4ec79
mansour-alhenaki <alhenaki.m.s@gmail.com>
2026-04-28 22:21:28 +0300

    Update config.py

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -14,8 +14,8 @@ ACCEPTED_DEGREES = [
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "").strip()
 HF_TOKEN = os.getenv("HF_TOKEN", "").strip()
 
-EMAILJS_SERVICE_ID          = os.getenv("EMAILJS_SERVICE_ID", "").strip()
-EMAILJS_TEMPLATE_ID         = os.getenv("EMAILJS_TEMPLATE_ID", "").strip()
-EMAILJS_TEMPLATE_ID_PASSWORD= os.getenv("EMAILJS_TEMPLATE_ID_PASSWORD", "").strip()
-EMAILJS_PUBLIC_KEY          = os.getenv("EMAILJS_PUBLIC_KEY", "").strip()
-EMAILJS_PRIVATE_KEY         = os.getenv("EMAILJS_PRIVATE_KEY", "").strip()
+EMAILJS_SERVICE_ID           = "service_jkytisf"
+EMAILJS_TEMPLATE_ID          = "template_k7xkcw1"
+EMAILJS_TEMPLATE_ID_PASSWORD = "template_n4x7r8o"
+EMAILJS_PUBLIC_KEY           = "_PCJUgH4omH5FwzUT"
+EMAILJS_PRIVATE_KEY          = "gLvYG8uXEPGxjuPmThEeI"
